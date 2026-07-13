bba13005a072e4e2587d56f3cb378e462b5f189c
jhonnatas <jhonnatas.aires@gmail.com>
2026-07-13 15:49:05 -0300

    feat:add JWT login endpoint

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from app.routers import user
+from app.routers import user, auth
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def read_root():
